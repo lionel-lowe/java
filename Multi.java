b4db2124a12c0f9e4d37cb54ca6f77c4dba4a005
lionel-lowe <lioneldjomolowe@gmail.com>
2025-11-06 20:06:26 +0100

    add Multi.java

---
+++ Multi.java
@@ -0,0 +1,9 @@
+public class Multi{
+    public static void aff(){
+        for (int i=1; i<10; i++){
+            for(int j=1; i<10; i++){
+                System.out.println(i+"x"+j+"="+i*j);
+            }
+        }
+    }
+}
