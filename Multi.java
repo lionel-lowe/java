247d5622290fc56000aadb05735f3b30c51de403
lionel-lowe <lioneldjomolowe@gmail.com>
2025-11-06 20:11:48 +0100

    return to the version with one table only

--- Multi.java
+++ Multi.java
@@ -1,7 +1,7 @@
 public class Multi{
     public static void aff(){
         for (int i=1; i<10; i++){
-            for(int j=1; j<10; j++){
+            for(int j=1; i<10; i++){
                 System.out.println(i+"x"+j+"="+i*j);
             }
         }
